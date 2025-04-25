177061ade2f2ee5d45984dab57b58e34e580c6f0
Tchakoumi <lorraintchakoumi@gmail.com>
2025-04-25 11:59:13 +0200

    feat: adjusted creation form to have placeholders and add role

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,18 +6,51 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            // ->add('roles')
-            ->add('password')
-            ->add('first_name')
-            ->add('last_name')
-        ;
+            ->add('email', EmailType::class, [
+                'attr' => [
+                    'placeholder' => 'Email',
+                    'class' => 'form-control',
+                ],
+            ])
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'User' => 'ROLE_USER',
+                    'Librarian' => 'ROLE_LIBRARIAN',
+                    'Admin' => 'ROLE_ADMIN',
+                ],
+                'multiple' => true,
+                'expanded' => true,
+            ])
+            ->add('password', PasswordType::class, [
+                'mapped' => false,
+                'attr' => [
+                    'placeholder' => 'Password',
+                    'class' => 'form-control',
+                ],
+
+            ])
+            ->add('first_name', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'First Name',
+                    'class' => 'form-control',
+                ],
+            ])
+            ->add('last_name', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'Last Name',
+                    'class' => 'form-control',
+                ],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
